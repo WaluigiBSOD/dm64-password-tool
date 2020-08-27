1b5aea9f9447a0038d5ebc05c7b96e2364e8d4f7
WaluigiBSOD <69278068+WaluigiBSOD@users.noreply.github.com>
2020-08-27 16:50:12 +0200

    Moved source code to the "src" folder

---
+++ src/PasswordMiscellaneous.h
@@ -0,0 +1,24 @@
+#ifndef PASSWORDMISCELLANEOUS_H_INCLUDED
+#define PASSWORDMISCELLANEOUS_H_INCLUDED
+
+// Dr. Mario 64 Password Encoder/Decoder Tool
+// Copyright (C) 2020  Waluigi BSOD (waluigibsod.github.io)
+//
+// This file is part of Dr. Mario 64 Password Encoder/Decoder Tool.
+//
+// Dr. Mario 64 Password Encoder/Decoder Tool is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Dr. Mario 64 Password Encoder/Decoder Tool is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+string _GenerateRandomPassword();
+
+#endif // PASSWORDMISCELLANEOUS_H_INCLUDED
